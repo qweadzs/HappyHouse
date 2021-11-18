a4d35ac15be575feae93aa51e3832dda2bab38a4
박현우 <qweadzs@naver.com>
2021-11-18 19:21:18 +0900

    asd

--- src/components/layout/NaviBar.vue
+++ src/components/layout/NaviBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">
         <router-link to="/">
           <img
