3724246046774906cc37fd333e98be16e47b8614
박현우 <qweadzs@naver.com>
2021-11-18 22:53:53 +0900

    .

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,24 +1,4 @@
-<template>
-  <v-footer absolute class="overflow-hidden" justify-center>
-    <div class="text-center mb-8">
-      <v-btn color="deep-purple" outlined @click="active = !active">
-        More
-      </v-btn>
-    </div>
-
-    <v-bottom-navigation v-model="value" :input-value="active" color="indigo">
-      <v-btn>
-        <span>커피 사주기</span>
-        <v-icon>mdi-coffee</v-icon>
-      </v-btn>
-
-      <v-btn>
-        <span>구독하기</span>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-    </v-bottom-navigation>
-  </v-footer>
-</template>
+<template></template>
 
 <script>
 export default {
