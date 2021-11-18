e46b68c570a0a824d72efe7c1ba64cd361101e0f
정광균 <rhkdrbs06@naver.com>
2021-11-18 21:39:39 +0900

    푸터

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,17 +1,30 @@
 <template>
   <v-footer color="primary lighten-1" padless>
-    <v-row align="center" justify="space-around">
-      <v-text-field
-        depressed
-        color="primary"
-        justify="center"
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
-      <v-btn depressed color="primary"> Primary </v-btn>
-    </v-row>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row align="">
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="이름"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="E-mail"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-btn elevation="2">구독하기</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
   </v-footer>
 </template>
 
