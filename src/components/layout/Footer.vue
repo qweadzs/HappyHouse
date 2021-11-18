14245ea43d5fd2f2b696da2d514eb8161fada469
정광균 <rhkdrbs06@naver.com>
2021-11-18 21:24:46 +0900

    footer 부탁 !!

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,42 +1,16 @@
 <template>
   <v-footer color="primary lighten-1" padless>
-    <v-row justify="center" no-gutters>
-      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-        <strong>
-          <v-btn :to="{ name: 'Home' }" color="white" text rounded class="my-2">
-            Home
-          </v-btn>
-          <v-btn
-            :to="{ name: 'Board' }"
-            color="white"
-            text
-            rounded
-            class="my-2"
-          >
-            게시판
-          </v-btn>
-          <v-btn
-            :to="{ name: 'House' }"
-            color="white"
-            text
-            rounded
-            class="my-2"
-          >
-            아파트정보
-          </v-btn>
-          <v-btn
-            :to="{ name: 'SignUp' }"
-            color="white"
-            text
-            rounded
-            class="my-2"
-          >
-            회원가입
-          </v-btn>
-          <br />
-          {{ new Date().getFullYear() }} — 서울_12반_정광균_박현우
-        </strong>
-      </v-col>
+    <v-row align="center" justify="space-around">
+      <v-text-field
+        depressed
+        color="primary"
+        justify="center"
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+      <v-btn depressed color="primary"> Primary </v-btn>
     </v-row>
   </v-footer>
 </template>
