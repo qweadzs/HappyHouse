be94747c086544cb2f8cfb34f99ba0375c76f257
정광균 <rhkdrbs06@naver.com>
2021-11-21 13:38:10 +0900

    클릭시 구독하기

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -21,19 +21,48 @@
 
     <v-btn>
       <span>구독하기</span>
-      <v-icon>mdi-heart</v-icon>
+      <v-icon @click="doSubscribe()">mdi-heart</v-icon>
     </v-btn>
   </v-bottom-navigation>
   <!-- </v-footer> -->
 </template>
 
 <script>
+import { mapState } from "vuex";
+import { registSub } from "@/api/member";
+const memberStore = "memberStore";
+
 export default {
+  name: "FooterBar",
+  computed: {
+    ...mapState(memberStore, ["isLogin", "userInfo"]),
+  },
   data: () => ({
     links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"],
     value: 1,
     active: false,
   }),
-  name: "FooterBar",
+  methods: {
+    doSubscribe() {
+      if (this.isLogin) {
+        registSub(
+          this.userInfo.userid,
+          ({ data }) => {
+            let msg = "등록 처리시 문제가 발생했습니다.";
+            if (data === "success") {
+              msg = "등록이 완료되었습니다.";
+            }
+            alert(msg);
+          },
+          (error) => {
+            alert("이미 구독 중 이십니다");
+            console.log(error);
+          }
+        );
+      } else {
+        alert("로그인을 해주세요 !!");
+      }
+    },
+  },
 };
 </script>
