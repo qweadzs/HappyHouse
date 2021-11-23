eacbe425584890cb7528d772f89636be1bb115ac
박현우 <qweadzs@naver.com>
2021-11-23 20:20:58 +0900

    메인페이지 수정

---
+++ src/components/home/HotDeal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div outlined>
+    <h2>
+      <div class="masthead-subheading mt-3">Hot Deal.</div>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HotDeal",
+};
+</script>
+
+<style>
+.masthead-subheading {
+  text-align: left;
+  font-size: 50px;
+  font-style: italic;
+  line-height: 1.5rem;
+  margin-bottom: 25px;
+  font-family: "Roboto Slab", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+</style>
+<style scoped></style>
---
+++ src/components/home/Info.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>
+      <div class="masthead-subheading mt-3">Info.</div>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Info",
+};
+</script>
+
+<style scoped></style>
