55518172eb91acdf88dc043e67f69a01f2b905e4
정광균 <rhkdrbs06@naver.com>
2021-11-24 19:20:41 +0900

    road

---
+++ src/components/house/Road.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>111</h2>
+</template>
+
+<script>
+import { mapState } from "vuex";
+const houseStore = "houseStore";
+
+export default {
+  name: "Road",
+  computed: {
+    ...mapState(houseStore, ["house"]),
+  },
+  watch: {
+    house: function () {},
+  },
+};
+</script>
+
+<style></style>
